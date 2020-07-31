23abe589bfd5c98629a69558362d195ac31db527
cl0v3r <haiclover@haiclover.local>
2020-07-31 18:13:23 +0700

    release

---
+++ README.md
@@ -0,0 +1,6 @@
+# Local brand list
+
+
+## 								Security Vulnerabilities
+
+If you discover a security vulnerability within my application, please send an e-mail to **Dinh Hai** via [haidv.work@gmail.com](mailto:haidv.work@gmail.com). All security vulnerabilities will be promptly addressed.
---
+++ js/popup.js
@@ -0,0 +1,779 @@
+let local_brand_list = {
+	"HNBMG": {
+		"image": "https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-9/60916288_2216397525081589_3506252459678892032_n.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=jLvnyJnXy8gAX8cXCao&_nc_ht=scontent.fhan2-4.fna&oh=6cb879fe4065df0e52dfbff592b7a80c&oe=5F47AFBA",
+		"instagram": "",
+		"fb" : "https://www.facebook.com/hnbmg/",
+		"web" : "https://hnbmg.com/"
+	},
+	"A R M O R Y": {
+		"image": "https://scontent.fhan2-2.fna.fbcdn.net/v/t1.0-9/87460269_2630315127200728_5369558312343830528_o.jpg?_nc_cat=111&_nc_sid=85a577&_nc_ohc=OENiHVrkPpAAX_YcrGL&_nc_ht=scontent.fhan2-2.fna&oh=652312a1607c4067cd45a752257f078b&oe=5F49188D",
+		"instagram": "",
+		"fb" : "https://www.facebook.com/armory0901/",
+		"web" : ""
+	},
+	"5theway": {
+		"image": "https://scontent.fhan2-3.fna.fbcdn.net/v/t1.0-9/107496686_1606405459522422_2132857329306576778_n.jpg?_nc_cat=1&_nc_sid=85a577&_nc_ohc=lvpWATDCDnsAX8I4InY&_nc_ht=scontent.fhan2-3.fna&oh=cc5a509e7f92fbcfd5ad44c3ba8138b5&oe=5F4A718C",
+		"instagram": "https://www.instagram.com/5thewayvietnam/?hl=vi",
+		"fb" : "https://www.facebook.com/5theway/",
+		"web" : ""
+	},
+	"1-OAK Shop": {
+		"image": "https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-9/18699816_1546216128743150_7412891022756088032_n.jpg?_nc_cat=105&_nc_sid=85a577&_nc_ohc=AKChEbrOZE8AX_mpTZF&_nc_ht=scontent.fhan2-4.fna&oh=ba112632a8a3e9c576f80d5433492cdb&oe=5F4B6431",
+		"instagram": "https://www.instagram.com/1oak.club/",
+		"fb" : "https://www.facebook.com/1OAKSHOP",
+		"web" : "http://1oak.vn/"
+	},
+	"13. LÌMID": {
+		"image": "https://scontent.fhan2-5.fna.fbcdn.net/v/t1.0-9/86719745_2096685603810504_4234517469554802688_n.jpg?_nc_cat=107&_nc_sid=85a577&_nc_ohc=WgT2Gy8YwJEAX-yQBrJ&_nc_ht=scontent.fhan2-5.fna&oh=619a4ea59908acdbb69a5f720cbd2af7&oe=5F484D47",
+		"instagram": "https://www.instagram.com/13.limid/",
+		"fb" : "https://www.facebook.com/13.limid/",
+		"web" : "https://13limid.com/"
+	},
+	"3i Store": {
+		"image": "https://scontent.fhan2-2.fna.fbcdn.net/v/t1.0-9/84610111_1154084748130238_2613042241572175872_o.jpg?_nc_cat=106&_nc_sid=85a577&_nc_ohc=9MXOG21PvY4AX95N-Hy&_nc_ht=scontent.fhan2-2.fna&oh=53f3730ca173d09cbb099b1f7631df58&oe=5F4A1B8E",
+		"instagram": "",
+		"fb" : "https://www.facebook.com/threeistore/",
+		"web" : ""
+	},
+	"APride Store": {
+		"image": "https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-9/75635937_2671294736285974_2761571573696561152_n.jpg?_nc_cat=104&_nc_sid=85a577&_nc_ohc=iMG46UQKAZIAX-93Pxz&_nc_ht=scontent.fhan2-4.fna&oh=4180a823e8d16bf1a50c4cd52a93ff01&oe=5F4862CB",
+		"instagram": "https://www.instagram.com/aprideclothing/",
+		"fb" : "https://www.facebook.com/aprideclothings/",
+		"web" : "https://apride.vn/"
+	},
+	"Abyss": {
+		"image": "https://scontent.fhan2-5.fna.fbcdn.net/v/t1.0-9/115821997_156624229381352_8720403395711687622_o.png?_nc_cat=109&_nc_sid=85a577&_nc_ohc=s97tXGCFoG8AX-paaB0&_nc_ht=scontent.fhan2-5.fna&oh=c84d6c4145dbb6402f2c4ee9f2551f7d&oe=5F49F941",
+		"instagram": "",
+		"fb" : "https://www.facebook.com/permalink.php?id=101892581521184&story_fbid=157048106005631",
+		"web" : ""
+	},
+	"Anti Monday": {
+		"image": "https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-9/105952915_3495172120512352_7477370069887335227_o.jpg?_nc_cat=105&_nc_sid=85a577&_nc_ohc=O6n3dj1KDoMAX9gDOTr&_nc_ht=scontent.fhan2-4.fna&oh=c003480502d15e2b8c55232c6b6ce857&oe=5F4A8124",
+		"instagram": "",
+		"fb" : "https://www.facebook.com/antimondaybrand",
+		"web" : "https://antimonday.vn/"
+	},
+	"Anti Trendy Trendy Boys": {
+		"image": "https://scontent.fhan2-5.fna.fbcdn.net/v/t31.0-8/17191967_1132331520228323_2648944074690245122_o.jpg?_nc_cat=107&_nc_sid=85a577&_nc_ohc=du9RNba9NP0AX9xSGq0&_nc_ht=scontent.fhan2-5.fna&oh=92c97bc0b2f9755d85fbce616c88501f&oe=5F495806",
+		"instagram": "https://www.instagram.com/AntiTrendyTrendyBoys",
+		"fb" : "https://www.facebook.com/AntiTrendyTrendyBoys/",
+		"web" : ""
+	},
+	"Aokaie": {
+		"image": "https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-9/90114643_1926805924117967_6750606536726282240_o.png?_nc_cat=110&_nc_sid=85a577&_nc_ohc=YOVg2LPxi9EAX_fMXDl&_nc_ht=scontent.fhan2-4.fna&oh=80fa6ecd5b0671781a5854879d08f5ea&oe=5F48A647",
+		"instagram": "https://www.instagram.com/aokaie_clothing/",
+		"fb" : "https://www.facebook.com/aokaieclothing/",
+		"web" : ""
+	},
+	"ALL 'BOUT HIM": {
+		"image": "https://scontent.fhan2-1.fna.fbcdn.net/v/t1.0-9/48407701_2151796431737832_2217950575130050560_o.jpg?_nc_cat=102&_nc_sid=85a577&_nc_ohc=yReREO5qTfUAX-4Bm1I&_nc_ht=scontent.fhan2-1.fna&oh=c9e4d22ab22a67c960d5b4305a6db0fe&oe=5F47C45D",
+		"instagram": "https://www.instagram.com/aahmidnightclub/?hl=vi",
+		"fb" : "https://www.facebook.com/aahmidnightclub/",
+		"web" : "https://aah-midnightclub.com/"
+	},
+	"AOB": {
+		"image": "https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-9/65935013_2406217526315037_252216070352404480_o.jpg?_nc_cat=110&_nc_sid=85a577&_nc_ohc=O0YT6yARoyYAX9V5dm0&_nc_ht=scontent.fhan2-4.fna&oh=b3c2d6297553c2fc5dfbfaecc910d5cd&oe=5F48DBFB",
+		"instagram": "",
+		"fb" : "https://www.facebook.com/AOB-1570330109903787/",
+		"web" : ""
+	},
+	"Almost Blue": {
+		"image": "https://scontent.fhan2-6.fna.fbcdn.net/v/t1.0-9/45015624_2076580592405020_3946840306665652224_n.jpg?_nc_cat=103&_nc_sid=85a577&_nc_ohc=_vR2O2uVNb8AX8lrK8a&_nc_ht=scontent.fhan2-6.fna&oh=3ede315f5bd261427b7fe2f87b0d966d&oe=5F48B6F9",
+		"instagram": "https://www.instagram.com/almostbluexx/",
+		"fb" : "https://www.facebook.com/Almostbluexx",
+		"web" : ""
+	},
+	"BB STREET WEAR": {
+		"image": "https://scontent.fhan2-5.fna.fbcdn.net/v/t1.0-9/83284013_2375000029384001_2807118383315681280_n.jpg?_nc_cat=107&_nc_sid=85a577&_nc_ohc=KVy6EOSN1WEAX8ElUYb&_nc_ht=scontent.fhan2-5.fna&oh=474191e5449823bdb5d5c28cd286afaf&oe=5F49B7DA",
+		"instagram": "https://www.instagram.com/bbsw.sg/",
+		"fb" : "https://www.facebook.com/bbstreetwear/",
+		"web" : ""
+	},
+	"BLVCK": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Bloomode": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"BOBUI STORE": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Bad habits": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Billion Dreamers": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"BWAB": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"BegudB": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Centimental": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Chillin'": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Callme": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Century Clothing": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"ClownZ Streetwear": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"DVRK": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Dirty Coins": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : "https://dirtycoins.vn/"
+	},
+	"DarKdust": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Dadmade": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Destroy": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Degrey": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : "https://degrey.vn/"
+	},
+	"DRAGON": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Deereus": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Dear Heart": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"ĐẬU FUU": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Easy n' Fun Store": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Exit": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"FREAKERS": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Fusionism": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Fomo Essential mix": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Fearless Clothing": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Felica": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"G.O Streetwear": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"GEEK GEAR": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Gekko": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"HFWTH": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"HBS ": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Hanoi Boyz Swagg": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Hurricane Closet": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Headless": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"HUDB CLUB": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"HADES": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Heaven": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"illusion": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"IX / XII": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Kiaat": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Lèng The Fabulous": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Lazythink": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"LIDER": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"MOLLY CLO": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Mezzy club": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Mane": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Madeby Py": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Môi Điên": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Named": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Nirvana Streetwear": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"NOW": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Off-On-Off": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Play Dirty": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Peace United Store": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"PIT Clothing": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"RETRO KID Streetwear": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"RAREclub": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"RanDol Clothing": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Rumor": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"SCAIT": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"$maker": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"SSStutter": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Shadow": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"SWE": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"ST WEAR": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Swag.Family": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Snazzy Clothing": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"SCVLE": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"T.P.C": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Tum Machines": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"TSUN": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Tobi Streetwear": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"The Maze XXX": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"The LOCAL Saigon": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"The Noir": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"The CloBrary": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"This is BITE": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"TEDS": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"T-22": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"The Jacket": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"The Dark Inside": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"The Brothers": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"The Eye Of Ra": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"The Originals": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"The Vandal": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Urban Monkey$": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Vibeism Streetwear": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"When I'm Nasty": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Young Lucy": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Yellow Flicker / YF": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Zup store": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Bama": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Birdy bag": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Underwater": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"The Beuter": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Saigon Swagger": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"DIM": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Back to Basic": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"The LOCAL Saigon": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"HYPE.vn": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Jamlos": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"Túi": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"ĐẬM": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"ToteTalk": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	},
+	"RieNevan": {
+		"image": "",
+		"instagram": "",
+		"fb" : "",
+		"web" : ""
+	}
+};
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function setHtml(list){
+	let border_color_list = ["red", "orange", "yellow", "olive", "green", "teal", "blue", "violet", "purple", "pink", "brown", "grey", "black"];
+	let final_html = "";
+	let name = "", fb = "", web = "";
+	for (const [key, value] of Object.entries(list)) {
+		name = key;
+		image = (value.image === "") ? "/images/default.png" : value.image;
+	  	fb = (value.fb === "") ? "#" : value.fb;
+	  	instagram = (value.instagram === "") ? "#" : value.instagram;
+		web = (value.web === "") ? "#" : value.web;
+		border_color = border_color_list[getRandomInt(0,13)];
+		default_html = '<div class="' + border_color + ' card"> <div class="ui small image"> <img src="' + image + '"/> </div> <div class="content"> <div class="center aligned header">' + name + '</div></div><div class="extra content"> <div class="center aligned"> <a href="' + fb + '" target="_blank"><i class="facebook icon"></i></a> <a href="' + instagram + '" target="_blank"><i class="instagram icon"></i> </a> <a href="' + web + '" target="_blank"><i class="linkify icon"></i></a> </div></div></div>';
+		final_html += default_html;
+	}
+	return final_html;
+}
+
+$("#local_brand_list").html(setHtml(local_brand_list));
+
+$("#search").change(function(){
+	let new_list = {};
+	for (const [key, value] of Object.entries(local_brand_list)) {
+		let new_key = key.toLowerCase();
+		let new_input = $(this).val().toLowerCase();
+		if(new_key.indexOf(new_input) != -1){
+			new_list[key] = value;
+		}
+	}
+	$("#local_brand_list").html(setHtml(new_list));
+});
+
